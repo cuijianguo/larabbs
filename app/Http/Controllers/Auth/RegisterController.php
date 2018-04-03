68240bacb8085edcac3db39c8020382bc6dfa953
vagrant <vagrant@homestead>
2018-04-03 08:43:32 +0000

    Add captcha

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -34,38 +34,42 @@ class RegisterController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct ()
     {
-        $this->middleware('guest');
+        $this->middleware ( 'guest' );
     }
 
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+    protected function validator ( array $data )
     {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+        return Validator::make ( $data , [
+            'name'     => 'required|string|max:255' ,
+            'email'    => 'required|string|email|max:255|unique:users' ,
+            'password' => 'required|string|min:6|confirmed' ,
+            'captcha'  => 'required|captcha' ,
+        ] , [
+            'captcha.required' => '验证码不能为空' ,
+            'captcha.captcha'  => '请输入正确的验证码' ,
+        ] );
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function create ( array $data )
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
+        return User::create ( [
+            'name'     => $data[ 'name' ] ,
+            'email'    => $data[ 'email' ] ,
+            'password' => bcrypt ( $data[ 'password' ] ) ,
+        ] );
     }
 }
