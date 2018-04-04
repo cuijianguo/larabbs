e4d7a069aae3281582368551546de2b5325a604c
vagrant <vagrant@homestead>
2018-04-04 03:24:43 +0000

    User can upload avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name' , 'email' , 'password' ,
-        'introduction'
+        'introduction' ,
+        'avatar'
     ];
 
     /**
