3d463e4afa4f4e6369298d0c494168feff9cf2f1
vagrant <vagrant@homestead>
2018-04-10 07:53:24 +0000

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,6 +6,11 @@ class Topic extends Model
 {
     protected $fillable = [ 'title' , 'body' , 'category_id' , 'excerpt' , 'slug' ];
 
+    public function replies ()
+    {
+        return $this->hasMany ( Reply::class );
+    }
+
     public function category ()
     {
         return $this->belongsTo ( Category::class );
@@ -45,8 +50,8 @@ class Topic extends Model
         return $query->orderBy ( 'created_at' , 'desc' );
     }
 
-    public function link($params = [])
+    public function link ( $params = [] )
     {
-        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+        return route ( 'topics.show' , array_merge ( [ $this->id , $this->slug ] , $params ) );
     }
 }
