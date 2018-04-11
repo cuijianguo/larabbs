699c89f64bc4eed1687ba3c005a587ae76a92aeb
vagrant <vagrant@homestead>
2018-04-11 05:44:30 +0000

    Admin permissions

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,15 @@ class PagesController extends Controller
     {
         return view ( 'pages.root' );
     }
+
+    public function permissionDenied ()
+    {
+        // 如果当前用户有权限访问后台，直接跳转访问
+        $callback = config ( 'administrator.permission' );
+        if ( $callback() ) {
+            return redirect ( url ( config ( 'administrator.uri' ) ) , 302 );
+        }
+        // 否则使用视图
+        return view ( 'pages.permission_denied' );
+    }
 }
