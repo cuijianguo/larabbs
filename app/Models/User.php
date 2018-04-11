a961a84fae14ff9ecd5c36123da8dbc9971abfc8
vagrant <vagrant@homestead>
2018-04-11 09:19:01 +0000

    Keep track of user last visited time

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use App\Models\Traits\ActiveUserHelper;
+use Carbon\Carbon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -15,6 +17,7 @@ class User extends Authenticatable
     }
     use HasRoles;
     use ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
 
 
     /**
@@ -91,5 +94,32 @@ class User extends Authenticatable
         $this->notification_count = 0;
         $this->save ();
         $this->unreadNotifications->markAsRead ();
+    }
+
+    public function syncUserActivedAt ()
+    {
+        // 获取昨天的日期，格式如：2017-10-21
+        $yesterday_date = Carbon::yesterday ()->toDateString ();
+
+        // Redis 哈希表的命名，如：larabbs_last_actived_at_2017-10-21
+        $hash = $this->hash_prefix . $yesterday_date;
+
+        // 从 Redis 中获取所有哈希表里的数据
+        $dates = Redis::hGetAll ( $hash );
+
+        // 遍历，并同步到数据库中
+        foreach ( $dates as $user_id => $actived_at ) {
+            // 会将 `user_1` 转换为 1
+            $user_id = str_replace ( $this->field_prefix , '' , $user_id );
+
+            // 只有当用户存在时才更新到数据库中
+            if ( $user = $this->find ( $user_id ) ) {
+                $user->last_actived_at = $actived_at;
+                $user->save ();
+            }
+        }
+
+        // 以数据库为中心的存储，既已同步，即可删除
+        Redis::del ( $hash );
     }
 }
