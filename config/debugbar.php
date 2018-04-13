cb478ffa69e776f986b8efde611bfcf1a7d6e973
vagrant <vagrant@homestead>
2018-04-13 03:42:59 +0000

    fix laravel-debugbar

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,10 +14,10 @@ return [
      |
      */
 
-    'enabled' => env('DEBUGBAR_ENABLED', true),
-    'except' => [
+    'enabled' => env ( 'DEBUGER_ENABLE' , false ) ,
+    'except'  => [
         //
-    ],
+    ] ,
 
     /*
      |--------------------------------------------------------------------------
@@ -32,12 +32,12 @@ return [
      |
      */
     'storage' => [
-        'enabled'    => true,
-        'driver'     => 'file', // redis, file, pdo, custom
-        'path'       => storage_path('debugbar'), // For file driver
-        'connection' => null,   // Leave null for default connection (Redis/PDO)
+        'enabled'    => true ,
+        'driver'     => 'file' , // redis, file, pdo, custom
+        'path'       => storage_path ( 'debugbar' ) , // For file driver
+        'connection' => null ,   // Leave null for default connection (Redis/PDO)
         'provider'   => '' // Instance of StorageInterface for custom driver
-    ],
+    ] ,
 
     /*
      |--------------------------------------------------------------------------
@@ -53,7 +53,7 @@ return [
      |
      */
 
-    'include_vendors' => true,
+    'include_vendors' => true ,
 
     /*
      |--------------------------------------------------------------------------
@@ -66,8 +66,8 @@ return [
      | Optionally, you can also send ServerTiming headers on ajax requests for the Chrome DevTools.
      */
 
-    'capture_ajax' => true,
-    'add_ajax_timing' => false,
+    'capture_ajax'    => true ,
+    'add_ajax_timing' => false ,
 
     /*
      |--------------------------------------------------------------------------
@@ -78,8 +78,8 @@ return [
      | in the Messages tab.
      |
      */
-    'error_handler' => false,
-    
+    'error_handler'   => false ,
+
     /*
      |--------------------------------------------------------------------------
      | Clockwork integration
@@ -89,7 +89,7 @@ return [
      | Extension, without the server-side code. It uses Debugbar collectors instead.
      |
      */
-    'clockwork' => false,
+    'clockwork'       => false ,
 
     /*
      |--------------------------------------------------------------------------
@@ -101,28 +101,28 @@ return [
      */
 
     'collectors' => [
-        'phpinfo'         => true,  // Php version
-        'messages'        => true,  // Messages
-        'time'            => true,  // Time Datalogger
-        'memory'          => true,  // Memory usage
-        'exceptions'      => true,  // Exception displayer
-        'log'             => true,  // Logs from Monolog (merged in messages if enabled)
-        'db'              => true,  // Show database (PDO) queries and bindings
-        'views'           => true,  // Views with their data
-        'route'           => true,  // Current route information
-        'auth'            => true, // Display Laravel authentication status
-        'gate'            => true, // Display Laravel Gate checks
-        'session'         => true,  // Display session data
-        'symfony_request' => true,  // Only one can be enabled..
-        'mail'            => true,  // Catch mail messages
-        'laravel'         => false, // Laravel version and environment
-        'events'          => false, // All events fired
-        'default_request' => false, // Regular or special Symfony request logger
-        'logs'            => false, // Add the latest log messages
-        'files'           => false, // Show the included files
-        'config'          => false, // Display config settings
-        'cache'           => false, // Display cache events
-    ],
+        'phpinfo'         => true ,  // Php version
+        'messages'        => true ,  // Messages
+        'time'            => true ,  // Time Datalogger
+        'memory'          => true ,  // Memory usage
+        'exceptions'      => true ,  // Exception displayer
+        'log'             => true ,  // Logs from Monolog (merged in messages if enabled)
+        'db'              => true ,  // Show database (PDO) queries and bindings
+        'views'           => true ,  // Views with their data
+        'route'           => true ,  // Current route information
+        'auth'            => true , // Display Laravel authentication status
+        'gate'            => true , // Display Laravel Gate checks
+        'session'         => true ,  // Display session data
+        'symfony_request' => true ,  // Only one can be enabled..
+        'mail'            => true ,  // Catch mail messages
+        'laravel'         => false , // Laravel version and environment
+        'events'          => false , // All events fired
+        'default_request' => false , // Regular or special Symfony request logger
+        'logs'            => false , // Add the latest log messages
+        'files'           => false , // Show the included files
+        'config'          => false , // Display config settings
+        'cache'           => false , // Display cache events
+    ] ,
 
     /*
      |--------------------------------------------------------------------------
@@ -134,35 +134,35 @@ return [
      */
 
     'options' => [
-        'auth' => [
-            'show_name' => true,   // Also show the users name/email in the debugbar
-        ],
-        'db' => [
-            'with_params'       => true,   // Render SQL with the parameters substituted
-            'backtrace'         => true,   // Use a backtrace to find the origin of the query in your files.
-            'timeline'          => false,  // Add the queries to the timeline
-            'explain' => [                 // Show EXPLAIN output on queries
-                'enabled' => false,
-                'types' => ['SELECT'],     // ['SELECT', 'INSERT', 'UPDATE', 'DELETE']; for MySQL 5.6.3+
-            ],
-            'hints'             => true,    // Show hints for common mistakes
-        ],
-        'mail' => [
+        'auth'  => [
+            'show_name' => true ,   // Also show the users name/email in the debugbar
+        ] ,
+        'db'    => [
+            'with_params' => true ,   // Render SQL with the parameters substituted
+            'backtrace'   => true ,   // Use a backtrace to find the origin of the query in your files.
+            'timeline'    => false ,  // Add the queries to the timeline
+            'explain'     => [                 // Show EXPLAIN output on queries
+                'enabled' => false ,
+                'types'   => [ 'SELECT' ] ,     // ['SELECT', 'INSERT', 'UPDATE', 'DELETE']; for MySQL 5.6.3+
+            ] ,
+            'hints'       => true ,    // Show hints for common mistakes
+        ] ,
+        'mail'  => [
             'full_log' => false
-        ],
+        ] ,
         'views' => [
-            'data' => false,    //Note: Can slow down the application, because the data can be quite large..
-        ],
+            'data' => false ,    //Note: Can slow down the application, because the data can be quite large..
+        ] ,
         'route' => [
             'label' => true  // show complete route on bar
-        ],
-        'logs' => [
+        ] ,
+        'logs'  => [
             'file' => null
-        ],
+        ] ,
         'cache' => [
             'values' => true // collect cache values
-        ],
-    ],
+        ] ,
+    ] ,
 
     /*
      |--------------------------------------------------------------------------
@@ -175,7 +175,7 @@ return [
      |
      */
 
-    'inject' => true,
+    'inject'       => true ,
 
     /*
      |--------------------------------------------------------------------------
@@ -187,7 +187,7 @@ return [
      | from trying to overcome bugs like this: http://trac.nginx.org/nginx/ticket/97
      |
      */
-    'route_prefix' => '_debugbar',
+    'route_prefix' => '_debugbar' ,
 
     /*
      |--------------------------------------------------------------------------
@@ -197,5 +197,5 @@ return [
      | By default DebugBar route served from the same domain that request served.
      | To override default domain, specify it as a non-empty value.
      */
-    'route_domain' => null,
+    'route_domain' => null ,
 ];
