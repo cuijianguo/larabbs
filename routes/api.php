139f5dac4ac0af5ede549a00c36b046a4b1a18f1
cuijianguo <785662290@qq.com>
2018-04-20 09:58:45 +0800

    notifications index

--- routes/api.php
+++ routes/api.php
@@ -76,8 +76,8 @@ $api->version ( 'v1' , [
         $api->get ( 'topics/{topic}/replies' , 'RepliesController@index' )
             ->name ( 'api.topics.replies.index' );
         // 某个用户的回复列表
-        $api->get('users/{user}/replies', 'RepliesController@userIndex')
-            ->name('api.users.replies.index');
+        $api->get ( 'users/{user}/replies' , 'RepliesController@userIndex' )
+            ->name ( 'api.users.replies.index' );
 
 
         // 需要 token 验证的接口
@@ -106,6 +106,10 @@ $api->version ( 'v1' , [
             // 删除回复
             $api->delete ( 'topics/{topic}/replies/{reply}' , 'RepliesController@destroy' )
                 ->name ( 'api.topics.replies.destroy' );
+
+            // 通知列表
+            $api->get ( 'user/notifications' , 'NotificationsController@index' )
+                ->name ( 'api.user.notifications.index' );
         } );
     } );
 } );
