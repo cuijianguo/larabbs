14cf898d73315232491300983dcfbad861d657d8
cuijianguo <785662290@qq.com>
2018-04-20 10:01:48 +0800

    notifications stats

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -9,10 +9,17 @@ use App\Transformers\NotificationTransformer;
 
 class NotificationsController extends Controller
 {
-    public function index()
+    public function index ()
     {
-        $notifications = $this->user->notifications()->paginate(20);
+        $notifications = $this->user->notifications ()->paginate ( 20 );
 
-        return $this->response->paginator($notifications, new NotificationTransformer());
+        return $this->response->paginator ( $notifications , new NotificationTransformer() );
+    }
+
+    public function stats ()
+    {
+        return $this->response->array ( [
+            'unread_count' => $this->user ()->notification_count ,
+        ] );
     }
 }
