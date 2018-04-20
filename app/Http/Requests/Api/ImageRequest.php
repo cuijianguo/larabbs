16625cfe81efbb88e798caee2bb6d477c464bf5e
cuijianguo <785662290@qq.com>
2018-04-20 11:41:22 +0800

    locale

--- app/Http/Requests/Api/ImageRequest.php
+++ app/Http/Requests/Api/ImageRequest.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
 
 class ImageRequest extends FormRequest
 {
-    public function authorize ()
-    {
-        return true;
-    }
 
     public function rules ()
     {
